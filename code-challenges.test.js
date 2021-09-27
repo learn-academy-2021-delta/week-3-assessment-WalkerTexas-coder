7ad0852286b23ed92816d2a7bba2703c0d080b80
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-09-27 12:08:01 -0700

    assessment review

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,16 @@
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
+// lets make a test
+//describe block
+ describe("fib", ()=>{
+     // it block
+     it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", ()=>{
+         //expect statments
+         expect(fib(6)).toEqual([1, 1, 2, 3, 5, 8])
+         expect(fib(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+     })
+ })
 
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -24,12 +34,61 @@
 
 
 // b) Create the function that makes the test pass.
+//PSEUDO CODE
+// Create the function that takes in a number as an arugment
+const fib = (number) => {
+    // if a number is less than two return "enter a number greater than one"
+    if(number < 2) {
+        return "please enter a number greater than two"
+    }
+    // declare an array called sequence and assign values for index 0 and index 1 [1,1]
+    let sequence = [1,1]
+    // establish an a array with length equal to the input. 
+        // for loop according to the number input
+        //for loop starts at two
+        for (let index = 2; index < number; index++) {
+            // nextNum = sequence[index - 1] + sequence[index - 2]
+            let nextNum = sequence[index - 1] + sequence[index - 2]
+            // sequence.push(nextNum)
+         sequence.push(nextNum)
+        }
+    // return sequence
+    return sequence
+}
 
+// let numOne = 1
+// let numTwo = 1
+// let nextNum
 
+// const fib = (number) => {
+//     let sequence = []
+//     if(number < 2){
+//         return number
+//     }
+//     else{
+//         for (let i = 0; i < number; i++) {
+//             sequence.push(numOne)
+//             nextNum = numOne + numTwo
+//             numOne = numTwo
+//             numTwo = nextNum
+//         }
+//         return sequence
+//     }
+// }
+
+// declare variable numberOne = 1
+// declare variable numberTwo = 1
+// declare variable nextNum
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("oddOrder", ()=>{
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", ()=>{
+        expect(oddOrder(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(oddOrder(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -39,12 +98,27 @@ var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 
 // b) Create the function that makes the test pass.
+    const oddOrder = (array) =>{
+        // declare function oddOrder paramter is an array
+        // declare variable to filter out numbers type of number and x % 2 != 0
+        let oddArray = array.filter(value => typeof value === "number" && value % 2 !== 0)
+        // return an array using sort method
+        return oddArray.sort((a,b) => a-b)
+    }
 
 
+    
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("sum", ()=>{
+    it("takes in an array and returns an array of the accumulating sum", ()=>{
+        expect(sum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(sum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(sum(numbersToAdd3)).toEqual([])
+    })
+})
 
 var numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -58,3 +132,34 @@ var numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+
+
+// declare a function that takes in an array as an arg
+
+
+// inline .map() solution
+    //  let sum = (array) => array.map((sum => value => sum += value)(0))
+    
+// for loop solution 
+    // const sum = (array) => {
+    //     let sumArray = []
+    //     let sumOf = 0
+    //     if(array.length === 0) {
+    //         return sumArray
+    //     }
+    //     for (let i = 0; i < array.length; i++) {
+    //        sumOf += array[i]
+    //        sumArray.push(sumOf)
+    //     }
+    //     return sumArray
+    // }
+   
+// foEach Solution
+    // const accumulator = (array) => {
+    //     sum = 0
+    //     array.forEach((element, index) => {
+    //         array[index] = element + sum
+    //         sum = element + sum
+    //     });
+    //     return arr
+    // }
--- interview-questions.md
+++ interview-questions.md
@@ -11,7 +11,8 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: JS keyword that refers to the object or class it belongs too. 
+  ex. this = ownerObject 
 
 
 
@@ -19,7 +20,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: Everything in react is a component which are independent and reusable pieces of code. React is a js library introduced by Facebook. Helps developers maintain functionality with scaling users and project code. Becuase of react states we can render / rerender individual components. Used industry wide. Open source. App creates simplified and specialized manipulation of the DOM by creating a virtual DOM in the memory.
 
 
 
@@ -27,7 +28,9 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: render(), allows us to display JSX using a return to the user. 
+  
+  optional: constructor(), super(),  componentWillMount(), componentDidMount(), compoenentWillUpdate(), componentDidUpdate(), componentWillUnmount()
 
 
 
@@ -35,7 +38,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: JSX JavaScript XML is Synatitcal Sugar for react. className versus class, ref. a CSS class name. JSX returns one element at a time (can use a React fragment to wrap all other elements). HTML can remain independent-- JSX must be included in JavaScript. Any element can be written as a self closing tag in JSX. 
 
 
 
@@ -43,25 +46,27 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: JS package manager- Tells node_modules how to behave. Yet Another Resource Negotiator- Facebook. Node_modules, package.json, yarn.lock. 
+  package.json: object that holds all of our dependencies 
+  yarn.lock: lists/ locks down the versions of different packages for version control on different machines. 
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: function w/o a name. function(),  ()=>{}
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: React's way of toggling between true and false conditions on the user interface. allows the user to render different components based on a condition. Making a log out button visible when the user is logged in - and vis versa. 
 
-2. Props:
+2. Props: used to pass information from one component from to another in react.
 
-3. JavaScript Events:
+3. JavaScript Events: JS has events that provide a dynamic interface- connected to the DOM. onClick - onLoad -onMouseOver -onChange
 
-4. Object oriented programming:
+4. Object oriented programming: OOP that utilizes classes and methods. Reusable code to structure projects. Creates OBject based elements that are held in hierarchy, methods belong to and manipulate the objects. 
 
-5. Ruby:
+5. Ruby: OOP used for backend server programming; Elegant & Dynamic Open source programming language. Created by Matz-- early 90s
